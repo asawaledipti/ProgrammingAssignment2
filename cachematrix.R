e45e515c283df76bc8a4c3bd9f29bc7cd8fecadb
asawaledipti <dipti.asawale2013@srisriuniversity.edu.in>
2014-08-23 03:17:39 +0530

    EDIT

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Inverse of matrix using Cached memory
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {  ## set the value of the vector
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x    ## get the value of the vector
-  setsolve <- function(mean) m <<- solve  ## set the value of the mean
-  getsolve <- function() m  ## get the value of the mean
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+        m <- NULL
+        set <- function(y) {  ## set the value of the vector
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x    ## get the value of the vector
+        setsolve <- function(solve) m <<- solve  ## set the value of the inverse
+        getsolve <- function() m  ## get the value of the inverse of a matrix
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  m <- x$getsolve()
-  if(!is.null(m)) {  
-    message("getting cached data")
-    return(m)  ## return the value if inverse already been calculated
-  }
-  data <- x$get()
-  m <- solve(data, ...) 
-  x$setsolve(m)
-  m  ## calculate the inverse if the value is not calculated before
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {  
+                message("getting cached data")
+                return(m)  ## return the value if inverse already been calculated
+        }
+        data <- x$get()
+        m <- solve(data, ...) 
+        x$setsolve(m)
+        m  ## calculate the inverse if the value is not calculated before
 }
